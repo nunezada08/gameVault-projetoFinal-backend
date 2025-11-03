dbae81b2b49eb980375f41c8a5990a8ff2593c98
lucas <lucastavaresbento@gmail.com>
2025-11-03 08:07:59 -0300

    feat: Finaliza getAll e getById

--- src/routes/jogosRoute.js
+++ src/routes/jogosRoute.js
@@ -4,5 +4,6 @@ import * as jogosController from '../controllers/jogosController.js';
 const router = Router()
 
 router.get("/", jogosController.listarTodosJogos);
+router.get("/:id", jogosController.listarUm);
 
 export default router;
